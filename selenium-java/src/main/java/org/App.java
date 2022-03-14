da5becd91b2300769af9b9c9db92326a08964b56
Filipe Morais <afilipem@gmail.com>
2022-03-13 21:52:22 -0500

    adds the code for the webdriver

--- selenium-java/src/main/java/org/App.java
+++ selenium-java/src/main/java/org/App.java
@@ -1,5 +1,8 @@
 package org;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        // Setting the chromedriver path
+        System.setProperty("webdriver.chrome.driver", "C:\\1.installers\\chromedriver_win32\\chromedriver.exe");
+
+        //Invoking browser
+        WebDriver driver = new ChromeDriver();
+        {
+            
+        };
     }
 }
